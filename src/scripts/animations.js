899a71d3416c96829c81b344e5d63777dc4ce522
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-03-31 22:12:28 +0800

    initial landing animation

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -3,8 +3,8 @@ import { ScrollScene } from 'scrollscene';
 
 const parallax = () => {
   const parallaxTimeline = gsap.timeline({ paused: true });
-  const parallaxNode = document.querySelector('.parallax');
-  const triggerNode = document.querySelector('.parallax__trigger');
+  const parallaxNode = document.querySelector('.animate__parallax');
+  const triggerNode = document.querySelector('.animate__parallax--trigger');
 
   // Properly set the bottom of the parallax element so that when the bottom of the
   // page is reached, the parallax element will still reach, even after the transform.
@@ -25,5 +25,38 @@ const parallax = () => {
   });
 };
 
+const show = () => {
+  const showNodes = document.querySelectorAll('.animate__show');
+
+  showNodes.forEach((item) => {
+    const showTimeline = gsap.timeline({ paused: true });
+    const opacity = item.dataset.opacity ? item.dataset.opacity : 0.5;
+    console.log(item);
+    showTimeline.from(item.children, {
+      y: 200,
+      opacity,
+      stagger: 0.05,
+      duration: 1,
+    });
+
+    const triggerHook = item.dataset.triggerhook
+      ? item.dataset.triggerhook
+      : 0.75;
+
+    // eslint-disable-next-line
+    const scrollScene = new ScrollScene({
+      triggerElement: item,
+      gsap: { timeline: showTimeline, reverse: false },
+      triggerHook,
+      reverse: false,
+    });
+
+    console.log(scrollScene);
+  });
+};
+
 // Global Animations
-document.addEventListener('DOMContentLoaded', () => parallax());
+document.addEventListener('DOMContentLoaded', () => {
+  parallax();
+  show();
+});
