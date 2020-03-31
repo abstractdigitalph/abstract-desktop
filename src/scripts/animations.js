6c601ad27cf302d829573be03cf3e87f0aac0b4c
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-03-31 19:55:59 +0800

    added spenmo

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,17 +1,29 @@
 import { gsap } from 'gsap';
 import { ScrollScene } from 'scrollscene';
 
-const parallaxTimeline = gsap.timeline({ paused: true });
-const parallaxNode = document.querySelector('.parallax');
-const triggerNode = document.querySelector('.parallax__trigger');
+const parallax = () => {
+  const parallaxTimeline = gsap.timeline({ paused: true });
+  const parallaxNode = document.querySelector('.parallax');
+  const triggerNode = document.querySelector('.parallax__trigger');
 
-parallaxTimeline.to(parallaxNode, { y: '-3000px', ease: 'linear' });
+  // Properly set the bottom of the parallax element so that when the bottom of the
+  // page is reached, the parallax element will still reach, even after the transform.
+  // The scrollHeight is used so that the speed of the element will be consistent between
+  // the different pages
+  const length = triggerNode.scrollHeight / -1.5;
+  gsap.set(parallaxNode, { bottom: length });
+  parallaxTimeline.to(parallaxNode, {
+    y: length,
+    ease: 'linear',
+  });
 
-document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line
   const scrollScene = new ScrollScene({
     triggerElement: triggerNode,
     gsap: { timeline: parallaxTimeline },
     duration: triggerNode.scrollHeight,
   });
-});
+};
+
+// Global Animations
+document.addEventListener('DOMContentLoaded', () => parallax());
