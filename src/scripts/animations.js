92bff5d0bfc5e2cb809b9e248899fe061ac648fb
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-04-06 19:53:05 +0800

    initial smooth scroll

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,121 +1,70 @@
 import { gsap } from 'gsap';
-import { ScrollScene } from 'scrollscene';
-import fullpage from 'fullpage.js';
 
-const parallax = () => {
-  const parallaxTimeline = gsap.timeline({ paused: true });
-  const parallaxNode = document.querySelector('.animate__parallax');
-  const triggerNode = document.querySelector('.animate__parallax--trigger');
+const SmoothScroll = (targetClass) => {
+  const { documentElement: html, body } = document;
 
-  // Properly set the bottom of the parallax element so that when the bottom of the
-  // page is reached, the parallax element will still reach, even after the transform.
-  // The scrollHeight is used so that the speed of the element will be consistent between
-  // the different pages
-  const length = triggerNode.scrollHeight / -1.5;
-  gsap.set(parallaxNode, { bottom: length });
-  parallaxTimeline.to(parallaxNode, {
-    y: length,
-    ease: 'linear',
-  });
+  // Object to hold information about the current page
+  const scroller = {
+    target: document.querySelector(targetClass),
+    ease: 0.05,
+    endY: 0,
+    y: 0,
+    resizeRequest: 1,
+    scrollRequest: 0,
+  };
 
-  // eslint-disable-next-line
-  const scrollScene = new ScrollScene({
-    triggerElement: triggerNode,
-    gsap: { timeline: parallaxTimeline },
-    duration: triggerNode.scrollHeight,
-    /*
-    controller: {
-      addIndicators: true,
-    },
-    */
-  });
-};
+  // Store return value of requestAnimationFrame()
+  let requestId = null;
 
-const show = () => {
-  const showNodes = document.querySelectorAll('.animate__show');
-
-  showNodes.forEach((item) => {
-    const showTimeline = gsap.timeline({ paused: true });
-    const layer = item.dataset.layer ? item.dataset.layer * 100 : 100;
-    const { direction } = item.dataset;
-    if (direction === 'up') {
-      showTimeline.from(item, {
-        y: layer,
-        ease: 'linear',
-      });
-    } else if (direction === 'down') {
-      showTimeline.to(item, {
-        y: -layer,
-        ease: 'linear',
-      });
-    } else {
-      showTimeline.fromTo(
-        item,
-        {
-          y: layer,
-          ease: 'linear',
-        },
-        {
-          y: -layer,
-          ease: 'linear',
-        },
-      );
+  // Function to update the scroller. Handles if the page is scrolled or resized
+  const updateScroller = () => {
+    const resized = scroller.resizeRequest;
+    if (resized) {
+      const height = scroller.target.clientHeight;
+      body.style.height = `${height}px`;
+      scroller.resizeRequest = 0;
     }
 
-    const triggerHook = item.dataset.triggerhook
-      ? item.dataset.triggerhook
-      : 0.75;
+    const scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
 
-    const triggerElement = item.dataset.parent ? item.parentNode : item;
+    scroller.endY = scrollY;
+    scroller.y += (scrollY - scroller.y) * scroller.ease;
 
-    // eslint-disable-next-line
-    const scrollScene = new ScrollScene({
-      triggerElement,
-      gsap: { timeline: showTimeline },
-      triggerHook,
-      duration: '100%',
+    if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+      scroller.y = scrollY;
+      scroller.scrollRequest = 0;
+    }
+
+    gsap.set(scroller.target, {
+      y: -scroller.y,
     });
-  });
+
+    requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+  };
+
+  function onScroll() {
+    scroller.scrollRequest += 1;
+    if (!requestId) {
+      requestId = requestAnimationFrame(updateScroller);
+    }
+  }
+
+  function onResize() {
+    scroller.resizeRequest += 1;
+    if (!requestId) {
+      requestId = requestAnimationFrame(updateScroller);
+    }
+  }
+
+  const onLoad = () => {
+    updateScroller();
+    window.focus();
+    window.addEventListener('resize', onResize);
+    window.addEventListener('scroll', onScroll);
+  };
+  window.addEventListener('load', onLoad);
+
+  return scroller;
 };
 
-const reveal = () => {
-  const revealNodes = document.querySelectorAll('.animate__reveal');
-
-  revealNodes.forEach((item) => {
-    const revealTimeline = gsap.timeline({ paused: true });
-    revealTimeline.from(item, { y: '200px', opacity: 0, ease: 'sine' });
-
-    const triggerElement = item.dataset.self ? item : item.parentNode;
-    const triggerHook = item.dataset.self ? 0.75 : 0.5;
-    // eslint-disable-next-line
-    const revealScene = new ScrollScene({
-      triggerElement,
-      triggerHook,
-      gsap: { timeline: revealTimeline },
-      scene: {
-        reverse: false,
-      },
-    });
-  });
-};
-
-const loadAnimations = () => {
-  const fullpageNode = document.querySelector('.animate__fullpage');
-
-  // eslint-disable-next-line
-  const fullpageInstance = new fullpage(fullpageNode, {
-    licenseKey: 'test',
-    verticalCentered: false,
-    scrollBar: true,
-    scrollingSpeed: 1500,
-  });
-
-  parallax();
-  show();
-  reveal();
-};
-
-// Global Animations
-document.addEventListener('DOMContentLoaded', () => {
-  loadAnimations();
-});
+const scroller = SmoothScroll('.smoothScroll__container');
