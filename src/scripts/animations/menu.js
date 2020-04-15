c537bd8d2f4b039cbbce2f381700be353ea5c216
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-04-16 00:20:49 +0800

    added contact us section

--- src/scripts/animations/menu.js
+++ src/scripts/animations/menu.js
@@ -7,7 +7,10 @@ const middleNode = hamburgerNode.querySelector('.hamburger__bar--middle');
 const bottomNode = hamburgerNode.querySelector('.hamburger__bar--bottom');
 const navNode = document.querySelector('.mainNav');
 const dribbbleNode = document.querySelector('.overlay__dribbble');
+const contactNode = document.querySelector('.contact');
+const contactTriggerNode = document.querySelectorAll('.contact__trigger');
 
+// Timeline to animate hamburger. Turning it to or from an x
 const hamburgerTimeline = gsap
   .timeline({
     paused: true,
@@ -20,17 +23,20 @@ const hamburgerTimeline = gsap
   .to(topNode, { rotate: 45 })
   .to(middleNode, { rotate: 45 }, '<')
   .to(bottomNode, { rotate: -45 }, '<')
-  .set({}, {}, 1.7);
+  .set({}, {}, 1.2);
 
+// Removes the dribbble logo from the left sidebar. Workaround as z-indexing
+// the dribbble logo to cover it up with the various overlays will not work
 const removeDribbleTimeline = gsap
   .timeline({ paused: true })
   .to(dribbbleNode, { opacity: 0, ease: 'power1.out', duration: 0.2 })
-  .set({}, {}, 1.7);
+  .set({}, {}, 1.2);
 
+// Animate opening and closing the timeline
 const menuTimeline = gsap
   .timeline({
     paused: true,
-    defaults: { duration: 1.5, ease: 'bounce' },
+    defaults: { duration: 1, ease: 'bounce' },
   })
   .fromTo(
     navNode,
@@ -43,29 +49,47 @@ const menuTimeline = gsap
     duration: 0.2,
   });
 
-const closeMenu = () => {
-  menuTimeline.reverse();
+const contactTimeline = gsap
+  .timeline({
+    paused: true,
+    defaults: { ease: 'power3.out' },
+  })
+  .from(contactNode, { xPercent: -100, duration: 1 });
+
+// Closes the respective timeline
+const close = (timeline) => {
+  timeline.reverse();
   removeDribbleTimeline.reverse();
   hamburgerTimeline.reverse();
   hamburgerNode.dataset.active = 'false';
 };
 
-const openMenu = () => {
+// Opens the respective timeline
+const open = (timeline, type) => {
   hamburgerTimeline.play();
   removeDribbleTimeline.play();
-  menuTimeline.play();
-  hamburgerNode.dataset.active = 'menu';
+  timeline.play();
+  hamburgerNode.dataset.active = type;
 };
-const startMenuAnimation = () => {
+
+// Animates the various parts depending on the
+const menuAnimation = () => {
   const { active } = hamburgerNode.dataset;
   switch (active) {
     case 'menu':
-      closeMenu();
+      close(menuTimeline);
+      break;
+    case 'contact':
+      close(contactTimeline);
       break;
     case 'closed':
     default:
-      openMenu();
+      open(menuTimeline, 'menu');
       break;
   }
 };
-hamburgerNode.addEventListener('click', startMenuAnimation);
+
+contactTriggerNode.forEach((element) => {
+  element.addEventListener('click', () => open(contactTimeline, 'contact'));
+});
+hamburgerNode.addEventListener('click', menuAnimation);
